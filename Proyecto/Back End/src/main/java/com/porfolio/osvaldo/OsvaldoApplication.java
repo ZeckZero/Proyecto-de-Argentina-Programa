1653eb27f2c72f37f1e7b13c9d2fdef9dfb488ab
ZeckZero <109942589+ZeckZero@users.noreply.github.com>
2023-01-02 15:27:57 -0300

    Back

---
+++ Proyecto/Back End/src/main/java/com/porfolio/osvaldo/OsvaldoApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.osvaldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsvaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsvaldoApplication.class, args);
+	}
+
+}
---
+++ Proyecto/Back End/src/test/java/com/porfolio/osvaldo/OsvaldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.osvaldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsvaldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
