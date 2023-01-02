e9e1562a0c372642fa7104947f8faa9d920d29c5
ZeckZero <109942589+ZeckZero@users.noreply.github.com>
2023-01-02 14:27:49 -0300

    Front end
    Front end terminado

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillComponent } from './components/skill/skill.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    SkillComponent,
+    ProyectoComponent,
+    RedesComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
