3f7094ef10adc167cb853be61e05c63c6021128a
ZeckZero <109942589+ZeckZero@users.noreply.github.com>
2023-01-10 02:44:51 -0300

    Back
    BackEnd

--- Proyecto/Front End/src/app/app.module.ts
+++ Proyecto/Front End/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillComponent } from './components/skill/skill.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +23,15 @@ import { RedesComponent } from './components/redes/redes.component';
     SkillComponent,
     ProyectoComponent,
     RedesComponent,
+    HomeComponent,
+    LoginComponent,
   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
